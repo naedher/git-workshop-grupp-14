5a65d16b9b14a01fd7e95eec053288fdd1279bb1
fredfrad <130115468+fredfrad@users.noreply.github.com>
2025-03-27 09:38:47 +0100

    Create main.java

---
+++ main.java
@@ -0,0 +1,10 @@
+
+
+import java.awt.image.BufferedImage;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner.Systemin();
+        String hej = scanner.nextline();
+        System.out.println(hej);
+}
